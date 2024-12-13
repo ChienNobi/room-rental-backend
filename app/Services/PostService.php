3ba3e27587a8e4064c8b3c0cc9712ce7ce866df6
Nob <tranthechien2012@gmail.com>
2024-12-13 21:57:36 +0700

    adding beauty treatment

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -18,6 +18,16 @@ class PostService extends BaseService
             $query->where('title', 'like', '%' . $params['title'] . '%');
         })->when(isset($params['status']), function ($query) use ($params) {
             $query->where('status', $params['status']);
+        })->when(isset($params['city']), function ($query) use ($params) {
+            $query->where('city', $params['city']);
+        })->when(isset($params['district']), function ($query) use ($params) {
+            $query->where('district', $params['district']);
+        })->when(isset($params['ward']), function ($query) use ($params) {
+            $query->where('ward', $params['ward']);
+        })->when(isset($params['room_type']), function ($query) use ($params) {
+            $query->where('room_type', $params['room_type']);
+        })->when(isset($params['price']), function ($query) use ($params) {
+            $query->whereBetween('rent_fee', $params['price']);
         });
     }
 }
